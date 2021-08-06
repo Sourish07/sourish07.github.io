82938406e7d4c294eedada3f33fa6002307a8330
Sourish Kundu <sourishkundu07@gmail.com>
2021-08-06 11:47:58 -0500

    Added template blog post and ready to deploy for testing

--- blog/js/script.js
+++ blog/js/script.js
@@ -1,21 +1,34 @@
 const nonTechnicalContent = $("#non-technical-content").detach();
 const technicalContent = $("#technical-content").detach();
-$(".blog-container").append(nonTechnicalContent);
+//$(".blog-container").append(nonTechnicalContent);
+const content = $(".content")
+content.html(nonTechnicalContent);
 
 $("#non-technical-link").on("click", function () {
     const nonTechnicalLink = $("#non-technical-link")
+    if (nonTechnicalLink.hasClass("reading-level-selected")) {
+        return
+    }
+
     nonTechnicalLink.addClass("reading-level-selected");
     nonTechnicalLink.removeClass("reading-level-unselected");
 
     const technicalLink = $("#technical-link");
     technicalLink.removeClass("reading-level-selected");
     technicalLink.addClass("reading-level-unselected");
+    content.fadeOut(200, function () {
+        content.html(nonTechnicalContent);
+    });
 
-    $(".content").html(nonTechnicalContent);
+    content.fadeIn();
 })
 
 $("#technical-link").on("click", function () {
     const technicalLink = $("#technical-link");
+    if (technicalLink.hasClass("reading-level-selected")) {
+        return
+    }
+
     technicalLink.addClass("reading-level-selected");
     technicalLink.removeClass("reading-level-unselected");
 
@@ -23,5 +36,8 @@ $("#technical-link").on("click", function () {
     nonTechnicalLink.removeClass("reading-level-selected");
     nonTechnicalLink.addClass("reading-level-unselected");
 
-    $(".content").html(technicalContent);
+    content.fadeOut(200, function () {
+        content.html(technicalContent);
+    });
+    content.fadeIn();
 })
