ff3065330155b30c81080bc6d905b87a943e958c
Sourish Kundu <sourishkundu07@gmail.com>
2021-08-09 22:42:31 -0500

    Removed sliding navbar at top

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,15 +1,15 @@
-var lastScrollTop = 0;
-navbar = document.getElementById("navbar");
-window.addEventListener("scroll", function () {
-    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    if (scrollTop > lastScrollTop) {
-        navbar.style.top = "-120px";
-    } else {
-        navbar.style.top = "0";
-    }
-    lastScrollTop = scrollTop;
-});
+// var lastScrollTop = 0;
+// navbar = document.getElementById("navbar");
+// window.addEventListener("scroll", function () {
+//     var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+//     if (scrollTop > lastScrollTop) {
+//         navbar.style.top = "-120px";
+//     } else {
+//         navbar.style.top = "0";
+//     }
+//     lastScrollTop = scrollTop;
+// });
 
 $(".sidebar-nav li").on("click", function () {
-    $(this).find("a")[0].click()
+    $(this).find("a")[0].click();
 })
