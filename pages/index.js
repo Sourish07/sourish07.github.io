233a8c860f1ec8d914c29828f150a03e0a26fd78
Sourish Kundu <sourishkundu07@gmail.com>
2023-01-24 19:20:03 -0600

    Fixed iPhone landscape white bars

--- pages/index.js
+++ pages/index.js
@@ -11,8 +11,9 @@ export default function Index() {
     return (
         <>
             <Head>
+                <html lang="en"/>
                 <title>Sourish's Personal Website</title>
-                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0 viewport-fit=cover" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             
