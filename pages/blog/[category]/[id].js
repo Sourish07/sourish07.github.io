87160beb52c88218f4c24be82f6144ff4d144246
Sourish Kundu <sourishkundu07@gmail.com>
2023-02-08 14:17:14 -0600

    Updated meta tags and thumbnails

--- pages/blog/[category]/[id].js
+++ pages/blog/[category]/[id].js
@@ -1,10 +1,11 @@
 import { getAllPostIds, getPostData } from '@/utils/processPosts';
 
 import Date from '@/components/blog/date';
+import Head from '@/components/blog/head';
 import Layout from '@/components/blog/layout';
-import Link from 'next/link';
 import blogStyles from '@/styles/blog/Blog.module.css';
 import styles from '@/styles/blog/Post.module.css';
+import Link from 'next/link';
 import { useState } from 'react';
 
 export function getStaticPaths() {
@@ -26,26 +27,30 @@ export async function getStaticProps({ params }) {
 
 export default function Post({ postData }) {
     return (
-        <Layout title={postData.title}>
-            <div className={styles.title}>{postData.title}</div>
-            <div className={styles.subheader}>{postData.subheader}</div>
-            <div className={styles.postInfo}>
-                <Date dateString={postData.date} />
-                <div style={{ display: "flex", alignItems: "center", width: "100%", marginTop: "5px" }}>
-                    {/* <div className={blogStyles.category} >
-                        {postData.category}
-                    </div> */}
-                    <Link href={`/blog/${postData.category}`} className={blogStyles.category}>
+        <>
+            <Head 
+                title={postData.title}
+                description={postData.subheader}
+                siteName={`sourish.dev/blog/${postData.category}/${postData.id}`}
+            />
+            <Layout>
+                <div className={styles.title}>{postData.title}</div>
+                <div className={styles.subheader}>{postData.subheader}</div>
+                <div className={styles.postInfo}>
+                    <Date dateString={postData.date} />
+                    <div style={{ display: "flex", alignItems: "center", width: "100%", marginTop: "5px" }}>
+                        <Link href={`/blog/${postData.category}`} className={blogStyles.category}>
                             {postData.category}
                         </Link>
-                    <div className={styles.author}>By Sourish Kundu</div>
+                        <div className={styles.author}>By Sourish Kundu</div>
+                    </div>
                 </div>
-            </div>
-            <div id="content" style={{width: "100%"}}>
-                <link rel="stylesheet" href="/blogAssets/css/code.css"/>
-                {postData.cspost ? cspost(postData) : <div className={styles.content} dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />}
-            </div>
-        </Layout>
+                <div id="content" style={{ width: "100%" }}>
+                    <link rel="stylesheet" href="/blogAssets/css/code.css" />
+                    {postData.cspost ? cspost(postData) : <div className={styles.content} dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />}
+                </div>
+            </Layout>
+        </>
     )
 }
 
@@ -55,11 +60,11 @@ export function cspost(postData) {
     return (
         <>
             <div className={styles.readingLevel}>
-                <div id="nonTechnicalLink" className={showNonTech ? styles.readingLevelSelected : styles.readingLevelUnselected} onClick={()=>{setShowNonTech(true); showNonTechnical()}}>Non-Technical</div>
-                <div id="technicalLink" className={showNonTech ? styles.readingLevelUnselected : styles.readingLevelSelected} onClick={()=>{setShowNonTech(false); showTechnical()}}>Technical</div>
+                <div id="nonTechnicalLink" className={showNonTech ? styles.readingLevelSelected : styles.readingLevelUnselected} onClick={() => { setShowNonTech(true); showNonTechnical() }}>Non-Technical</div>
+                <div id="technicalLink" className={showNonTech ? styles.readingLevelUnselected : styles.readingLevelSelected} onClick={() => { setShowNonTech(false); showTechnical() }}>Technical</div>
             </div>
             <div id="nonTechnicalContent" className={styles.content} dangerouslySetInnerHTML={{ __html: postData.nonTechnicalContent }} />
-            <div id="technicalContent" className={styles.content} style={{display: "none"}} dangerouslySetInnerHTML={{ __html: postData.technicalContent }} />
+            <div id="technicalContent" className={styles.content} style={{ display: "none" }} dangerouslySetInnerHTML={{ __html: postData.technicalContent }} />
         </>
     )
 }
@@ -90,8 +95,8 @@ function fadeOut(fade) {
     clearInterval(intervalID);
     var opacity = 1;
     fade.style.opacity = 1;
-    intervalID = setInterval(function() {
-  
+    intervalID = setInterval(function () {
+
         if (opacity > 0) {
             opacity = opacity - 0.02;
             fade.style.opacity = opacity;
@@ -105,8 +110,8 @@ function fadeIn(fade) {
     clearInterval(intervalID);
     var opacity = 0;
     fade.style.opacity = 0;
-    intervalID = setInterval(function() {
-  
+    intervalID = setInterval(function () {
+
         if (opacity < 1) {
             opacity = opacity + 0.02;
             fade.style.opacity = opacity;
--- pages/blog/[category]/index.js
+++ pages/blog/[category]/index.js
@@ -1,9 +1,10 @@
 import { getAllPostCategories, getPostsInCategory } from "@/utils/processPosts";
 
+import Head from '@/components/blog/head';
 import Layout from "@/components/blog/layout";
-import Link from "next/link";
 import PostList from "@/components/blog/postList";
 import styles from "@/styles/blog/Blog.module.css";
+import Link from "next/link";
 
 export function getStaticPaths() {
     const paths = getAllPostCategories();
@@ -25,10 +26,17 @@ export async function getStaticProps({ params }) {
 
 export default function BlogCategory({ category, postsData }) {
     return (
-        <Layout>
-            <h1 className={styles.pageTitle}>Category: {category}</h1>
-            <Link href="/blog" style={{width: "100%", textAlign: "end"}}>Back to all posts</Link>
-            <PostList posts={postsData} />
-        </Layout>
+        <>
+            <Head
+                title={`Category: ${category} - Sourish Shares`}
+                description="Here, I write about various topics about CS, the broader tech industry, and life."
+                siteName={`sourish.dev/blog/${category}`}
+            />
+            <Layout>
+                <h1 className={styles.pageTitle}>Category: {category}</h1>
+                <Link href="/blog" style={{ width: "100%", textAlign: "end" }}>Back to all posts</Link>
+                <PostList posts={postsData} />
+            </Layout>
+        </>
     );
 }
