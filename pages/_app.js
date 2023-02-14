a4d7cfb9f52e192906ec41bf8a4354be173ac59f
Sourish Kundu <sourishkundu07@gmail.com>
2023-02-14 08:27:31 -0600

    Added Vercel Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import '@/styles/globals.css';
 import "@fontsource/lato/100.css";
 import "@fontsource/lato/300.css";
 import Script from 'next/script';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
     return (
@@ -22,6 +23,7 @@ export default function App({ Component, pageProps }) {
                 )
             }
             <Component {...pageProps} />
+            <Analytics />
         </>
     )
 }
