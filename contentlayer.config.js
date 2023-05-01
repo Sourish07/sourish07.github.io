f1e74fe0331c86826a943f97645c0ca4168ba895
Sourish Kundu <sourishkundu07@gmail.com>
2023-04-30 22:29:13 -0500

    Fixed single quotes

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -1,53 +1,53 @@
-import { defineDocumentType, makeSource } from 'contentlayer/source-files'
-import remarkGfm from 'remark-gfm'
-import rehypeKatex from 'rehype-katex';
-import remarkMath from 'remark-math';
-import rehypePrettyCode from 'rehype-pretty-code';
-import rehypeHighlight from 'rehype-highlight';
+import { defineDocumentType, makeSource } from "contentlayer/source-files"
+import remarkGfm from "remark-gfm"
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
+import rehypePrettyCode from "rehype-pretty-code";
+import rehypeHighlight from "rehype-highlight";
 
 
 export const Post = defineDocumentType(() => ({
-  name: 'Post',
-  filePathPattern: `**/*.mdx`,
-  contentType: 'mdx',
+  name: "Post",
+  filePathPattern: "**/*.mdx",
+  contentType: "mdx",
   fields: {
     title: {
-      type: 'string',
-      description: 'The title of the post',
+      type: "string",
+      description: "The title of the post",
       required: true,
     },
     subheader: {
-      type: 'string',
-      description: 'The subheader of the post',
+      type: "string",
+      description: "The subheader of the post",
       required: true,
     },
     date: {
-      type: 'date',
-      description: 'The date of the post',
+      type: "date",
+      description: "The date of the post",
       required: true,
     },
     category: {
-      type: 'string',
-      description: 'The category of the post',
+      type: "string",
+      description: "The category of the post",
       required: true,
     },
     multilevelarticle: {
-      type: 'boolean',
-      description: 'Whether the post has multiple technical levels',
+      type: "boolean",
+      description: "Whether the post has multiple technical levels",
       required: false,
     }
   },
   computedFields: {
     url: {
-      type: 'string',
+      type: "string",
       resolve: (post) => `/blog/${post.category}/${post._raw.flattenedPath}`,
     },
     categoryUrl: {
-      type: 'string',
+      type: "string",
       resolve: (post) => `/blog/${post.category}`,
     },
     id: {
-      type: 'string',
+      type: "string",
       resolve: (post) => post._raw.flattenedPath,
     },
   },
@@ -55,7 +55,7 @@ export const Post = defineDocumentType(() => ({
 
 const options = {
   // Use one of Shiki's packaged themes
-  theme: 'one-dark-pro',
+  theme: "one-dark-pro",
  
   // Keep the background or use a custom background color?
   keepBackground: true,
@@ -66,21 +66,21 @@ const options = {
     // Prevent lines from collapsing in `display: grid` mode, and
     // allow empty lines to be copy/pasted
     if (node.children.length === 0) {
-      node.children = [{type: 'text', value: ' '}];
+      node.children = [{type: "text", value: " "}];
     }
   },
   onVisitHighlightedLine(node) {
     // Each line node by default has `class="line"`.
-    node.properties.className.push('highlighted');
+    node.properties.className.push("highlighted");
   },
   onVisitHighlightedWord(node) {
     // Each word node has no className by default.
-    node.properties.className = ['word'];
+    node.properties.className = ["word"];
   },
 };
 
 export default makeSource({
-  contentDirPath: 'posts',
+  contentDirPath: "posts",
   documentTypes: [Post],
   mdx: {
     remarkPlugins: [remarkMath, remarkGfm],
