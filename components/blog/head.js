7fac60e8685c1b7c4351a3a84bca2e92adfa2a72
Sourish Kundu <sourishkundu07@gmail.com>
2023-05-08 19:19:42 -0500

    Added keys to meta tags

--- components/blog/head.js
+++ components/blog/head.js
@@ -7,10 +7,10 @@ export default function BlogHead({ title, description, siteName, children }) {
             <title>{title}</title>
 
             <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
-            <meta property="site_name" content={siteName} />
-            <meta property="title" content={title} />
-            <meta property="description" content={description} />
-            <meta property="image" content={blogThumbnailPath} />
+            <meta property="site_name" content={siteName} key='site_name' />
+            <meta property="title" content={title} key='title' />
+            <meta property="description" content={description} key='description' />
+            <meta property="image" content={blogThumbnailPath} key='image' />
 
             <meta property="og:site_name" content={siteName} />
             <meta property="og:title" content={title} />
@@ -22,7 +22,7 @@ export default function BlogHead({ title, description, siteName, children }) {
             <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
             <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
             <link rel="manifest" href="/site.webmanifest" />
-            <meta name="msapplication-TileColor" content="#da532c" />
+            
             <meta name="theme-color" content="#ffffff" />
             {children}
         </Head>
--- components/main/head.js
+++ components/main/head.js
@@ -7,25 +7,23 @@ export default function SkHead({ title, children }) {
             <title>{title}</title>
 
             <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
-            
-            <meta property="site_name" content="sourish.dev" />
-            <meta property="title" content="Sourish's Personal Website" />
-            <meta property="description" content="Check out my website for a showcase of my skills and projects!" />
-            <meta property="image" content={thumbnailPath} />
+            <meta property="site_name" content="sourish.dev" key='site_name'/>
+            <meta property="title" content="Sourish's Personal Website" key='title'/>
+            <meta property="description" content="Check out my website for a showcase of my skills and projects!" key='description'/>
+            <meta property="image" content={thumbnailPath} key='image'/>
             
             <meta property="og:site_name" content="sourish.dev" />
             <meta property="og:title" content="Sourish's Personal Website" />
             <meta property="og:description" content="Check out my website for a showcase of my skills and projects!" />
             <meta property="og:image" content={thumbnailPath} />
 
-            <meta name="msapplication-TileColor" content="#da532c" />
-            <meta name="theme-color" content="#ffffff" />
+            <meta name="theme-color" content="#111111" />
             
+            <link rel="canonical" href="https://www.sourish.dev" />
             <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
             <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
             <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
             <link rel="manifest" href="/site.webmanifest" />
-            <link rel="canonical" href="https://www.sourish.dev" />
             {children}
         </Head>
     )
