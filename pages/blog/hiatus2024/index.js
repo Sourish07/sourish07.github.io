d26459fd342276ca8b331c4ef3eb4be84c509d2a
Sourish Kundu <sourishkundu07@gmail.com>
2024-11-16 01:46:45 -0800

    Pulling data from document

---
+++ pages/blog/hiatus2024/index.js
@@ -0,0 +1,72 @@
+import BlogHead from '@/components/blog/head';
+import Layout from "@/components/blog/layout";
+import styles from "@/styles/blog/Blog.module.css";
+import postStyles from "@/styles/blog/Post.module.css";
+import rehypeStringify from "rehype-stringify";
+import remarkGfm from "remark-gfm";
+import remarkParse from 'remark-parse';
+import remarkRehype from "remark-rehype";
+import useSWR from 'swr';
+import { unified } from 'unified';
+
+async function fetchNotes() {
+    const API_KEY = process.env.NEXT_PUBLIC_GOOGLE_SHEETS_API_KEY;
+    const documentId = "1Z3lUrw3VOf7TfE0CEiBEfCNrNKAOu-sQmt0xITShgEw";
+    const apiUrl = `https://www.googleapis.com/drive/v3/files/${documentId}/export?mimeType=text/markdown&key=${API_KEY}`;
+
+    // Make request
+    const res = await fetch(apiUrl);
+    const data = await res.text();
+    const content = await unified().use(remarkParse).use(remarkGfm).use(remarkRehype).use(rehypeStringify).process(data);
+    return content;
+}
+
+export default function Hiatus() {
+    const { data: notes, error } = useSWR("fetchNotes", fetchNotes);
+
+    return (
+        <>
+            <BlogHead title="Sourish's Hiatus Notes" >
+                <link rel="canonical" href="https://www.sourish.dev/blog/hiatus2024" />
+            </BlogHead>
+            <Layout>
+                <h1 className={styles.pageTitle}>Notes During 2024 Winter Hiatus</h1>
+                <div className={styles.subheader} style={{ marginBottom: "30px" }}>Progress updates about my hiatus.</div>
+                {!notes && (
+                    <div style={{ width: "100%" }}>
+                        <SkeletonLoaderPiece height="50px" />
+                        <SkeletonLoaderPiece width="20%" alignSelf="flex-end" />
+                        <SkeletonLoaderPiece />
+                        <SkeletonLoaderPiece width="20%" alignSelf="flex-end" />
+                    </div>
+                )}
+                {notes && <div className={postStyles.content} dangerouslySetInnerHTML={{ __html: notes }} style={{ width: "100%" }}></div>}
+                {error && <Error />}
+            </Layout>
+        </>
+    );
+}
+
+function Error() {
+    return (
+        <div style={{ width: "100%" }}>
+            <div style={{ fontSize: "35px", marginBottom: "10px", textAlign: "center", color: "red" }}>- Failed to fetch document -</div>
+        </div>
+    );
+}
+
+function SkeletonLoaderPiece({ width = '100%', height = '30px', borderRadius = '5px', alignSelf = 'flex-start' }) {
+    const style = {
+        width,
+        height,
+        borderRadius,
+        alignSelf,
+        backgroundColor: '#f0f0f0',
+        margin: '20px 0',
+        animation: 'flash 1.5s ease-in-out infinite',
+    };
+
+    return (
+        <div style={style}></div>
+    )
+};
