a3b52e4e5faa88667a2c9245c29fe754911f4abd
Sourish Kundu <sourishkundu07@gmail.com>
2024-12-02 21:39:11 -0800

    Updated next export

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const { withContentlayer } = require("next-contentlayer2")
 /** @type {import('next').NextConfig} */
 let nextConfig = {
   reactStrictMode: true,
+  output: 'export',
 }
 
 if (process.env.STATIC_BUILD) {
@@ -11,7 +12,7 @@ if (process.env.STATIC_BUILD) {
     unoptimized: true,
   }
   nextConfig.env = {
-    NEXT_PUBLIC_GITHUB_BUILD: true
+    NEXT_PUBLIC_GITHUB_BUILD: "true"
   }
 }
 
